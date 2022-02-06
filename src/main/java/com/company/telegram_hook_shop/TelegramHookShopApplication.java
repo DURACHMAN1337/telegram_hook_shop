9bbb9c3cb84186511714fa224feb3bd7a71fb9fe
s0mething <denguetta@gmail.com>
2022-02-06 15:54:17 +0400

    Initial commit

---
+++ src/main/java/com/company/telegram_hook_shop/TelegramHookShopApplication.java
@@ -0,0 +1,13 @@
+package com.company.telegram_hook_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramHookShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramHookShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/telegram_hook_shop/TelegramHookShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.telegram_hook_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramHookShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
